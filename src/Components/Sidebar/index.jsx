5e22c25f27fb82b79e4697ec40437ecc74f69629
akash-khan-dev <100846367+akash-khan-dev@users.noreply.github.com>
2023-04-01 00:38:23 +0600

    sidebar-desing-complete

--- src/Components/Sidebar/index.jsx
+++ src/Components/Sidebar/index.jsx
@@ -1,5 +1,39 @@
 import React from "react";
+import "./style.css";
+import avatar from "../../images/pngwing.com(1).png";
+import { AiOutlineHome } from "react-icons/ai";
+import { BiMessageRoundedDots } from "react-icons/bi";
+import { IoMdNotificationsOutline } from "react-icons/io";
+import { BsFillGearFill } from "react-icons/bs";
+import { AiOutlineLogin } from "react-icons/ai";
 
 export const Sidebar = () => {
-  return <div>index</div>;
+  return (
+    <>
+      <div className="side-bar">
+        <div className="Profile-img">
+          <picture>
+            <img src={avatar} alt="" />
+          </picture>
+        </div>
+        <div className="menubar-icon">
+          <div className="home-icon menu-icon">
+            <AiOutlineHome />
+          </div>
+          <div className="message-icon menu-icon">
+            <BiMessageRoundedDots />
+          </div>
+          <div className="colling-icon menu-icon">
+            <IoMdNotificationsOutline />
+          </div>
+          <div className="satting-icon menu-icon">
+            <BsFillGearFill />
+          </div>
+        </div>
+        <div className="logout-icon">
+          <AiOutlineLogin />
+        </div>
+      </div>
+    </>
+  );
 };
